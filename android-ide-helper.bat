fef8776d36b4f4f80067cba00b6a32a8a37e8eeb
Jasonnor <wujason810@gmail.com>
2020-04-23 00:38:15 +0800

    feature: android-ide-helper.bat

---
+++ android-ide-helper.bat
@@ -0,0 +1,15 @@
+:: Nox: First 62001, Second 62025, Third and so on 62026, 62027...
+adb connect 127.0.0.1:62001
+:: adb connect 127.0.0.1:62025
+adb connect 127.0.0.1:62026
+adb connect 127.0.0.1:62027
+adb connect 127.0.0.1:62028
+adb devices
+for /l %%x in (1, 1, 10) do (
+	adb -s 127.0.0.1:62026 forward tcp:12121 tcp:12121
+	:: adb -s 127.0.0.1:62028 forward tcp:12121 tcp:12121
+	adb -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
+	adb -s 127.0.0.1:62027 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62027 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
+	adb -s 127.0.0.1:62028 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62028 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
+	timeout /t 5
+)
