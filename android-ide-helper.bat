cffb492ebc986ff6644905f77ef7399f79cf5117
Jasonnor <wujason810@gmail.com>
2020-04-26 00:34:16 +0800

    feature: restart adb server before running ide-helper

--- android-ide-helper.bat
+++ android-ide-helper.bat
@@ -1,3 +1,5 @@
+adb kill-server
+adb start-server
 :: Nox: First 62001, Second 62025, Third and so on 62026, 62027...
 adb connect 127.0.0.1:62001
 :: adb connect 127.0.0.1:62025
@@ -6,10 +8,10 @@ adb connect 127.0.0.1:62027
 adb connect 127.0.0.1:62028
 adb devices
 for /l %%x in (1, 1, 10) do (
+	timeout /t 10
 	adb -s 127.0.0.1:62026 forward tcp:12121 tcp:12121
 	:: adb -s 127.0.0.1:62028 forward tcp:12121 tcp:12121
 	adb -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
 	adb -s 127.0.0.1:62027 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62027 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
 	adb -s 127.0.0.1:62028 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:62028 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
-	timeout /t 5
 )
