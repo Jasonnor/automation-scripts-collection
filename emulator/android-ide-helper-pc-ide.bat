bb4ec3c04783e7c4d19112878586b405c83ac113
Jasonnor <wujason810@gmail.com>
2021-01-03 18:02:52 +0800

    feature: create single ide helper script

---
+++ emulator/android-ide-helper-pc-ide.bat
@@ -0,0 +1,6 @@
+H:\Nox\Nox\bin\adb.exe kill-server
+H:\Nox\Nox\bin\adb.exe start-server
+H:\Nox\Nox\bin\adb.exe connect 127.0.0.1:62026
+H:\Nox\Nox\bin\adb.exe devices
+H:\Nox\Nox\bin\adb.exe -s 127.0.0.1:62026 forward tcp:12121 tcp:12121
+H:\Nox\Nox\bin\adb.exe -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | H:\Nox\Nox\bin\adb.exe -s 127.0.0.1:62026 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
