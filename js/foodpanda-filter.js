de7443e12876d4693498f89e1de85a0184470c28
Jasonnor <wujason810@gmail.com>
2021-09-09 22:19:38 +0800

    chore: fix typo of variables

--- js/foodpanda-filter.js
+++ js/foodpanda-filter.js
@@ -1,10 +1,10 @@
 let keepRules = ['享5折', '點5折', '45折', '65折', '$50', '買一送一']
-document.querySelectorAll('.vendor-list>li').forEach(function (vender) {
-    const venderTag = vender.querySelector('[data-testid=multi-tag__text]').textContent;
-    const categories = Array.apply(null, vender.querySelectorAll('.categories.summary>li>span'));
+document.querySelectorAll('.vendor-list>li').forEach(function (vendor) {
+    const vendorTag = vendor.querySelector('[data-testid=multi-tag__text]').textContent;
+    const categories = Array.apply(null, vendor.querySelectorAll('.categories.summary>li>span'));
     const notContainBeverage = categories.every(span => span.textContent !== '飲料');
-    if (notContainBeverage && new RegExp(keepRules.join('|')).test(venderTag)) {
+    if (notContainBeverage && new RegExp(keepRules.join('|')).test(vendorTag)) {
         return;
     }
-    vender.remove();
+    vendor.remove();
 })
