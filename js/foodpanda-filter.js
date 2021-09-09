48bf5766a5a362656ad5de51338e35bddd6acafb
Jasonnor <wujason810@gmail.com>
2021-09-09 18:28:22 +0800

    feature: remove vendor without multi-tag__text

--- js/foodpanda-filter.js
+++ js/foodpanda-filter.js
@@ -1,5 +1,9 @@
 let keepRules = ['享5折', '點5折', '45折', '65折', '$50', '買一送一']
 document.querySelectorAll('.vendor-list>li').forEach(function (vendor) {
+    if (vendor.querySelector('[data-testid=multi-tag__text]') === null) {
+        vendor.remove();
+        return;
+    }
     const vendorTag = vendor.querySelector('[data-testid=multi-tag__text]').textContent;
     const categories = Array.apply(null, vendor.querySelectorAll('.categories.summary>li>span'));
     const notContainBeverage = categories.every(span => span.textContent !== '飲料');
