566aa309cdeaa8f62cb7f083f5fedcdfd7efe7fb
Jasonnor <wujason810@gmail.com>
2021-09-09 20:54:44 +0800

    feature: set interval for foodpanda-filter.js

--- js/foodpanda-filter.js
+++ js/foodpanda-filter.js
@@ -1,14 +1,19 @@
-let keepRules = ['享5折', '點5折', '45折', '65折', '$50', '買一送一']
-document.querySelectorAll('.vendor-list>li').forEach(function (vendor) {
-    if (vendor.querySelector('[data-testid=multi-tag__text]') === null) {
+const intervalId = window.setInterval(function () {
+    let keepRules = ['享5折', '點5折', '45折', '65折', '$50', '買一送一']
+    document.querySelectorAll('.vendor-list>li').forEach(function (vendor) {
+        if (vendor.querySelector('[data-testid=multi-tag__text]') === null) {
+            vendor.remove();
+            return;
+        }
+        const vendorTag = vendor.querySelector('[data-testid=multi-tag__text]').textContent;
+        const categories = Array.apply(null, vendor.querySelectorAll('.categories.summary>li>span'));
+        const notContainBeverage = categories.every(span => span.textContent !== '飲料');
+        if (notContainBeverage && new RegExp(keepRules.join('|')).test(vendorTag)) {
+            return;
+        }
         vendor.remove();
-        return;
-    }
-    const vendorTag = vendor.querySelector('[data-testid=multi-tag__text]').textContent;
-    const categories = Array.apply(null, vendor.querySelectorAll('.categories.summary>li>span'));
-    const notContainBeverage = categories.every(span => span.textContent !== '飲料');
-    if (notContainBeverage && new RegExp(keepRules.join('|')).test(vendorTag)) {
-        return;
-    }
-    vendor.remove();
-})
+    })
+}, 1000);
+
+// To stop, use:
+clearInterval(intervalId)
