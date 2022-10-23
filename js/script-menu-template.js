7f6665a9c3e00beef85e46d10c2b04a7990c0d62
Jasonnor <wujason810@gmail.com>
2022-10-23 23:46:01 +0800

    feature: add rating count and food type filter

--- js/script-menu-template.js
+++ js/script-menu-template.js
@@ -16,27 +16,27 @@
     scriptDisplay.style.cursor = 'pointer'
     scriptDisplay.style.display = 'block'
     scriptDisplay.innerText = '🐳'
+    scriptBody.append(scriptDisplay)
     const scriptForm = document.createElement('div')
     scriptForm.id = 'script-form'
     scriptForm.style.display = 'none'
+    scriptBody.append(scriptForm)
     const scriptInputLabel = document.createElement('label')
     scriptInputLabel.setAttribute('for', 'script-input')
     scriptInputLabel.innerText = 'Input'
+    scriptForm.append(scriptInputLabel)
     const scriptInput = document.createElement('input')
     scriptInput.id = 'script-input'
     scriptInput.type = 'text'
+    scriptForm.append(scriptInput)
     const scriptButton = document.createElement('button')
     scriptButton.onclick = runScript
     scriptButton.innerText = 'Run'
-    scriptForm.append(scriptInputLabel)
-    scriptForm.append(scriptInput)
     scriptForm.append(scriptButton)
-    scriptBody.append(scriptDisplay)
-    scriptBody.append(scriptForm)
     document.body.append(scriptBody)
 
     const addCSS = s => (document.head.appendChild(document.createElement('style')).innerHTML = s)
-    addCSS('#script-form > * { margin: 5px }')
+    addCSS('#script-form > * { margin: 5px; display: block; }')
 
     function toggleScriptMenuDisplay() {
         const targetElement = document.getElementById('script-form')
