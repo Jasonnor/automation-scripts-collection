92197e4a833c3de4fb147a39603a54657dae5f66
Jasonnor <wujason810@gmail.com>
2026-08-23 22:32:16 +0800

    chore: clean unused scripts

--- emulator/android-ide-helper-al-ide.bat
+++
@@ -1,6 +0,0 @@
-adb kill-server
-adb start-server
-adb connect 127.0.0.1:16448
-adb devices
-adb -s 127.0.0.1:16448 forward tcp:12121 tcp:12121
-adb -s 127.0.0.1:16448 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver | adb -s 127.0.0.1:16448 shell sh /data/data/com.xxscript.idehelper/tengine/noroot/shellserver
